038e86f1022a8678b114db8f4236e0996dcbd4de
jn <jdnaranjo@uniboyaca.edu.co>
2022-09-12 20:34:24 -0500

    se agrego la funcionalidad de registrar contactos

--- js/index.js
+++ js/index.js
@@ -4,27 +4,56 @@ let contactos=[
      email:'mcamila@ejemplo.com',
      telefono:'123456'},
      {
-        Nombre: 'pepe rodriguez',
+        nombre: 'pepe rodriguez',
         email: 'test@test.com',
         telefono:'123456'
      }
 ]
 
-function MostrarContactos(){
+function mostrarContactos(){
     const tbody = document
     .getElementById('contactosTable')
     .getElementsByTagName('tbody')[0]
+    tbody.innerHTML = ''
     for(let index in contactos){
         const contacto = contactos[index]
         const id = parseInt(index)+1
         let fila='<tr>'
         fila += '  <td>'+id+'</td>'
-        fila += '  <td>'+contacto.Nombre+'</td>'
+        fila += '  <td>'+contacto.nombre+'</td>'
         fila += '  <td>'+contacto.email+'</td>'
         fila += '  <td>'+contacto.telefono+'</td>'
         fila +='</tr>'
         tbody.innerHTML += fila
     }
 }
+mostrarContactos()
 
-MostrarContactos()
+function cerrarModal(){
+    document.getElementById('formularioModal')
+    .classList.add('ocultarModal')
+    const formulario = document.forms['formContacto']
+    formulario.reset()
+}
+
+document.getElementById('cerrarModalBtn').addEventListener('click', () =>{
+    document.getElementById('formularioModal').classList.add('ocultarModal')
+})
+
+document.getElementById('registrarBtn').addEventListener('click',() =>{
+    document.getElementById('formularioModal')
+    .classList.remove('ocultarModal')
+})
+
+document.getElementById('formularioContacto').addEventListener('submit',(ev)=>{
+    ev.preventDefault()
+    //const nombre = document.getElementById('nombre').value
+    const contacto = {
+        nombre: document.forms['formContacto']['nombre'].value,
+        email: document.forms['formContacto']['email'].value,
+        telefono: document.forms['formContacto']['telefono'].value,
+    }
+    contactos.push(contacto)
+    cerrarModal()
+    mostrarContactos()
+})
